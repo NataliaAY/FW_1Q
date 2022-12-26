b8db0728afe61b13b5237d1df63aec448ecdaa39
Natalia <averegina@male.ru>
2022-12-26 21:33:43 +0300

    решена задача

---
+++ final1q/task/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int x)
+{
+    string[] output = new string[CountLessThan(input, x)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= x)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int n)
+{
+    int count = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] AskArray()
+{
+    Write("Введите любые символы через пробел: ");
+    return ReadLine().Split(" ");
+}
